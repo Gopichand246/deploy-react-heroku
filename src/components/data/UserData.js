6f0da87a8963b5cdc68894785e8b0d8eeb56e269
Gopichand <56589158+Gopichand246@users.noreply.github.com>
2022-08-12 10:28:38 +0530

    Update UserData.js

--- src/components/data/UserData.js
+++ src/components/data/UserData.js
@@ -2,8 +2,8 @@ const userData={
     "projects": [
         {
             "id": 1,
-            "title": "Smart Detection",
-            "tagLine" : "IoT Enabled Smart Fire Detection System",
+            "title": "Insurance Management System",
+            "tagLine" : "Insurance Management System",
             "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
             "Image": "/images/fireDetection.jpg",
             "startDate": "08-2020",
@@ -12,13 +12,13 @@ const userData={
                 "github": "https://github.com/Gopichand246/SmartDetection"
             },
             "MyContribution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-            "domain":"Internet of Things | Web Development",
-            "stack":['php','Arduino']
+            "domain":"Big Data Analytics",
+            "stack":['python','MySQL','PowerBI']
         },
         {
             "id": 2,
             "title": "Smart Detection",
-            "tagLine" : "Spy Surveillance Robotic System",
+            "tagLine" : "Bank Management System",
             "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
             "Image": "/images/surveillance.jpg",
             "startDate": "08-2020",
@@ -27,68 +27,8 @@ const userData={
                 "github": "https://github.com/Gopichand246/SmartDetection"
             },
             "MyContribution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-            "domain":"Internet of Things | Deep Learning",
-            "stack":['php','Arduino']
-        },
-        {
-            "id": 3,
-            "title": "Smart Detection",
-            "tagLine" : "Handwritten Signature Verification",
-            "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            "Image": "/images/pr.jpg",
-            "startDate": "08-2020",
-            "endDate": "09-2020",
-            "SourceLink": {
-                "github": "https://github.com/Gopichand246/SmartDetection"
-            },
-            "MyContribution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-            "domain":"Pattern Recognition",
-            "stack":['php','Arduino']
-        },
-        {
-            "id": 4,
-            "title": "Smart Detection",
-            "tagLine" : "Remote Controlled Robotic Arm",
-            "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            "Image": "/images/roboticArm.jpg",
-            "startDate": "08-2020",
-            "endDate": "09-2020",
-            "SourceLink": {
-                "github": "https://github.com/Gopichand246/SmartDetection"
-            },
-            "MyContribution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-            "domain":"3D Design",
-            "stack":['php','Arduino']
-        },
-        {
-            "id": 5,
-            "title": "Health Care",
-            "tagLine" : "Health Care",
-            "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            "Image": "/images/healthCare.jpg",
-            "startDate": "08-2020",
-            "endDate": "09-2020",
-            "SourceLink": {
-                "github": "https://github.com/Gopichand246/SmartDetection"
-            },
-            "MyContribution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-            "domain":"Web Development | Full Stack",
-            "stack":['php','Arduino']
-        },
-        {
-            "id": 6,
-            "title": "Content Aware Image Resizing",
-            "tagLine" : "Content Aware Image Resizing",
-            "Description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            "Image": "/images/dip.jpg",
-            "startDate": "08-2020",
-            "endDate": "09-2020",
-            "SourceLink": {
-                "github": "https://github.com/Gopichand246/SmartDetection"
-            },
-            "MyContribution": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
-            "domain":"Digital Image Processing",
-            "stack":['php','Arduino']
+            "domain":"Big Data Analytics",
+            "stack":['python','MySQL']
         }
     ],
     "About":{
@@ -96,15 +36,15 @@ const userData={
         "AboutText":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt"
     },
     "Contact":{
-        "mailId":"gopichand8990@gmail.com"
+        "mailId":"sushmithadogga@gmail.com"
     },
     "Education":[
         {
             "id":1,
             "degree":"Bachelor of Technology in Electronics and Communication Engineering",
-            "name":"Indian Institue of Information Technology Sricity, Chittoor",
-            "marks":"CGPA: 8.4 (6 semesters)",
-            "logo":"/images/iiits.png",
+            "name":"Andhra University College of Engineering for Women",
+            "marks":"CGPA: 8.11 (6 semesters)",
+            "logo":"",
             "Timeline":"2018-2022"
         },
         {
@@ -117,16 +57,16 @@ const userData={
         },
         {
             "id":3,
-            "degree":"Board of Secondary Education, Andhra Pradesh (BSEAP)",
-            "name":"Bhashyam Public School, Visakhapatnam",
-            "marks":"CGPA: 9.8",
-            "logo":"/images/bhashyam.png",
+            "degree":"Indian Council of Secondary Education (class 10)",
+            "name":"St.Ann's High school",
+            "marks":"Secured 93%",
+            "logo":"",
             "Timeline":"Upto 2016"
         }
     ],
     "Contact":
     {
-        'Email':'gopichand8990@gmail.com',
+        'Email':'sushmithadogga@gmail.com',
         'Mobile':'+91-6304399051',
         'Facebook':'https://www.facebook.com/gopichand.siripurapu/',
         'Instagram':'https://www.instagram.com/gopichand_s.g.c/',
